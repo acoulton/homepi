d007667f9d5959d779a76e3fbdb5c7083455013e
Andrew Coulton <andrew@ingenerator.com>
2014-01-27 23:37:48 +0000

    Remove ingenerator-php dependency for now

--- architecture/site-cookbooks/project/metadata.rb
+++ architecture/site-cookbooks/project/metadata.rb
@@ -5,8 +5,6 @@ license           "BSD"
 description       "Installs and configures the home raspberry pi monitoring"
 version           "0.0.1"
 
-depends "ingenerator-php"
-
 %w{ ubuntu debian }.each do |os|
   supports os
 end
