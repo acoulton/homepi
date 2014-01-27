002d93ff2fe2a04b7f9e264fdacda813914811e2
Andrew Coulton <andrew@ingenerator.com>
2014-01-27 23:17:17 +0000

    Manage GPIO modules in the kernel on boot
    Configure the thermo sensors to run

--- architecture/site-cookbooks/project/recipes/homepi.rb
+++ architecture/site-cookbooks/project/recipes/homepi.rb
@@ -1,3 +1,10 @@
 # Homepi recipe
 # To be refactored!
 
+# Activate IO kernel modules - note this will require a reboot after install
+cookbook_file "etc/modules" do
+  path "/etc/modules"
+  owner "root"
+  user  "root"
+  mode  0644
+end
